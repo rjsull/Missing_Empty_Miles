e92990f9385f4a0c087f4a4c7493910908727633
schnickens <34480861+schnickens@users.noreply.github.com>
2021-01-12 16:33:47 -0500

    Update and rename Missing Empty Miles 2021-01-06.sql to Missing Empty Miles.sql

--- Missing Empty Miles.sql
+++ Missing Empty Miles.sql
@@ -1,10 +1,33 @@
 DECLARE @DateStart DATETIME, 
 @DateEnd DATETIME, 
-@Branch VARCHAR(12)
+@Branch1 VARCHAR(12),
+@Branch2 VARCHAR(12),
+@Branch3 VARCHAR(12),
+@Branch4 VARCHAR(12),
+@Branch5 VARCHAR(12),
+@Branch6 VARCHAR(12),
+@Branch7 VARCHAR(12)
 
-SET @DateStart = '12-27-2020'  --DATEADD(dd, -(datepart(dw, GetDate())+7), GetDate())
-SET @DateEnd = '01-02-2021' --DATEADD(dd, -datepart(dw, GetDate()+1), GetDate())
-SET @Branch = '580' 
+
+SET @DateStart = 
+	CASE 
+	WHEN DATEPART(DW,GETDATE()) = 4 OR DATEPART(DW,GETDATE()) = 5 OR DATEPART(DW,GETDATE()) = 6 OR DATEPART(DW,GETDATE()) = 7 --On Wed, Thu, and Fri Mornings, send Start of Current Week
+	THEN DATEADD(wk, 0, DATEADD(DAY, 1-DATEPART(WEEKDAY, GETDATE()), DATEDIFF(dd, 0, GETDATE()))) --first day current week
+	ELSE DATEADD(wk, -1, DATEADD(DAY, 1-DATEPART(WEEKDAY, GETDATE()), DATEDIFF(dd, 0, GETDATE()))) --first day previous week
+	END
+SET @DateEnd = 	
+	CASE 
+	WHEN DATEPART(DW,GETDATE()) = 4 OR DATEPART(DW,GETDATE()) = 5 OR DATEPART(DW,GETDATE()) = 6  OR DATEPART(DW,GETDATE()) = 7 --On Wed, Thu, and Fri Mornings, send End of Current Week
+	THEN DATEADD(wk, 1, DATEADD(DAY, 0-DATEPART(WEEKDAY, GETDATE()), DATEDIFF(dd, 0, GETDATE()))) --last day current week
+	ELSE DATEADD(wk, 0, DATEADD(DAY, 0-DATEPART(WEEKDAY, GETDATE()), DATEDIFF(dd, 0, GETDATE()))) --last day previous week
+	END
+SET @Branch1 = '570'
+SET @Branch2 = '571'
+SET @Branch3 = '572'
+SET @Branch4 = '573'
+SET @Branch5 = '574'
+SET @Branch6 = '580'
+SET @Branch7 = '581'
 
 SELECT a.*
 FROM (
@@ -32,7 +55,7 @@ Consignee=cmp_id_rend,
 [Leg Miles] = lgh.lgh_miles, 
 [Ref#]=ord.ord_refnum, 
 [Update User]=lgh.lgh_updatedby, 
-[Last Update]=FORMAT(lgh.lgh_updatedon,'MM/dd/yyyy HH:mm'),
+[Last Update]=lgh.lgh_updatedon,
 [Fleet Manager] = mpp.mpp_teamleader,
 [MissingOrder] = 
 	CASE 
@@ -63,7 +86,7 @@ LEFT JOIN city ctyShip ON ord.ord_origincity = ctyShip.cty_code
 LEFT JOIN city ctyCon ON ord.ord_destcity = ctyCon.cty_code 
 LEFT JOIN manpowerprofile mpp ON lgh.lgh_driver1 = mpp.mpp_id
 WHERE CONVERT(varchar(10),lgh.lgh_startdate,102) BETWEEN '' + CONVERT(varchar(10),@DateStart,102) + '' AND '' + CONVERT(varchar(10),@DateEnd,102) + ''
-AND trc.trc_branch = @Branch
+AND trc.trc_branch IN (@Branch1, @Branch2, @Branch3, @Branch4, @Branch5, @Branch6, @Branch7)
 AND lgh.lgh_outstatus = 'CMP'
 --AND lgh.lgh_driver1 IN ('CLEAN')
 --ORDER BY lgh.lgh_driver1 ASC,
@@ -98,7 +121,7 @@ Consignee=cmp_id_rend,
 [Leg Miles] = lgh.lgh_miles, 
 [Ref#]=ord.ord_refnum, 
 [Update User]=lgh.lgh_updatedby, 
-[Last Update]=FORMAT(lgh.lgh_updatedon,'MM/dd/yyyy HH:mm'),
+[Last Update]=lgh.lgh_updatedon,
 [Fleet Manager] = mpp.mpp_teamleader,
 [MissingOrder] = 
 	CASE 
@@ -129,7 +152,7 @@ LEFT JOIN city ctyShip ON ord.ord_origincity = ctyShip.cty_code
 LEFT JOIN city ctyCon ON ord.ord_destcity = ctyCon.cty_code 
 LEFT JOIN manpowerprofile mpp ON lgh.lgh_driver1 = mpp.mpp_id
 WHERE CONVERT(varchar(10),lgh.lgh_startdate,102) BETWEEN '' + CONVERT(varchar(10),@DateStart,102) + '' AND '' + CONVERT(varchar(10),@DateEnd,102) + ''
-AND trc.trc_branch = @Branch
+AND trc.trc_branch IN (@Branch1, @Branch2, @Branch3, @Branch4, @Branch5, @Branch6, @Branch7)
 AND lgh.lgh_outstatus = 'CMP'
 )
 AS a
